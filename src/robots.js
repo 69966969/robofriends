b23d65770c88b8226664dee98148c00d947a7364
ArtisLotko <89071871+ArtisLotko@users.noreply.github.com>
2023-04-30 14:54:11 +0300

    Creating

---
+++ src/robots.js
@@ -0,0 +1,57 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Artis Lotko',
+        username: '699669',
+    },
+    {
+        id: 2,
+        name: 'Patricija',
+        username: 'pro',
+    },
+    {
+        id: 3,
+        name: 'Tomass Andersons',
+        username: 'Okhm777',
+    },
+    {
+        id: 4,
+        name: 'Andrejs Izģērbējs',
+        username: 'Ryyyyyzhais',
+    },
+    {
+        id: 5,
+        name: 'Māris Lotko',
+        username: 'V80',
+    },
+    {
+        id: 6,
+        name: 'Ausma Lotko',
+        username: 'Major',
+    },
+    {
+        id: 7,
+        name: 'Ēriks Jansons',
+        username: 'Cepējs',
+    },
+   {
+        id: 8,
+        name: 'Ričš',
+        username: 'Students',
+    },
+     {
+        id: 9,
+        name: 'Amanda',
+        username: '3',
+    },
+     {
+        id: 10,
+        name: 'Ivčiks',
+        username: 'nezinu',
+    },
+     {
+        id: 11,
+        name: 'Leny_LV',
+        username: 'GamerBoy',
+    }
+]
