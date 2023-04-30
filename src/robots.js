d6fa98c4a2cf553c322834a5d7f3402c04ccce02
ArtisLotko <artislotko69@gmail.com>
2023-04-30 16:52:10 +0300

    # removal

--- src/robots.js
+++ src/robots.js
@@ -2,62 +2,62 @@ export const robots = [
     {
         id: 1,
         name: 'Artis!',
-        username: '#3973',
+        username: '3973',
     },
     {
         id: 2,
         name: 'Sun Wukong',
-        username: '#0364',
+        username: '0364',
     },
     {
         id: 3,
         name: 'Pagerbejs',
-        username: '#1823',
+        username: '1823',
     },
     {
         id: 4,
         name: 'Aluxu',
-        username: '#7928'
+        username: '7928'
     },
     {
         id: 5,
         name: 'Eriks123',
-        username: '#3063',
+        username: '3063',
     },
     {
         id: 6,
         name: 'Memoria',
-        username: '#9191',
+        username: '9191',
     },
     {
         id: 7,
         name: 'Slowdive',
-        username: '#0782',
+        username: '0782',
     },
    {
         id: 8,
         name: 'Agares',
-        username: '#6986',
+        username: '6986',
     },
      {
         id: 9,
         name: 'ExampleLV',
-        username: '#7180',
+        username: '7180',
     },
      {
         id: 10,
         name: 'GLOBO',
-        username: '#2551',
+        username: '2551',
     },
      {
         id: 11,
         name: 'Leny_LV',
-        username: '#0519',
+        username: '0519',
     },
      {
         id: 12,
         name: 's1lent',
-        username: '#9616',
+        username: '9616',
     },
      {
         id: 13,
