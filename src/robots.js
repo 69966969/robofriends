440bc7f385e75dc42da06df20eb473293ffb75ff
ArtisLotko <artislotko69@gmail.com>
2023-04-30 16:48:31 +0300

    Robot update

--- src/robots.js
+++ src/robots.js
@@ -1,57 +1,67 @@
 export const robots = [
     {
         id: 1,
-        name: 'Artis Lotko',
-        username: '699669',
+        name: 'Artis!',
+        username: '#3973',
     },
     {
         id: 2,
-        name: 'Patricija',
-        username: 'pro',
+        name: 'Sun Wukong',
+        username: '#0364',
     },
     {
         id: 3,
-        name: 'Tomass Andersons',
-        username: 'Okhm777',
+        name: 'Pagerbejs',
+        username: '#1823',
     },
     {
         id: 4,
-        name: 'Andrejs Ortikovs',
-        username: 'Ryyyyyzhais',
+        name: 'Aluxu',
+        username: '#7928'
     },
     {
         id: 5,
-        name: 'Māris Lotko',
-        username: 'V80',
+        name: 'Eriks123',
+        username: '#3063',
     },
     {
         id: 6,
-        name: 'Ausma Lotko',
-        username: 'Major',
+        name: 'Memoria',
+        username: '#9191',
     },
     {
         id: 7,
-        name: 'Ēriks Jansons',
-        username: 'Cepējs',
+        name: 'Slowdive',
+        username: '#0782',
     },
    {
         id: 8,
-        name: 'Ričš',
-        username: 'Students',
+        name: 'Agares',
+        username: '#6986',
     },
      {
         id: 9,
-        name: 'Amanda',
-        username: '3',
+        name: 'ExampleLV',
+        username: '#7180',
     },
      {
         id: 10,
-        name: 'Ivčiks',
-        username: 'nezinu',
+        name: 'GLOBO',
+        username: '#2551',
     },
      {
         id: 11,
         name: 'Leny_LV',
-        username: 'GamerBoy',
+        username: '#0519',
+    },
+     {
+        id: 12,
+        name: 's1lent',
+        username: '#9616',
+    },
+     {
+        id: 13,
+        name: 'Okhm777',
+        username: '#8282',
     }
 ]
